98c3e711476bcb4f862f195398ed775e76c212e4
Mike Wild <Michael.Wild@defra.onmicrosoft.com>
2025-05-28 09:57:19 +0100

    Add Cosmos db to app host

--- src/Rag.Chat.AppHost/Program.cs
+++ src/Rag.Chat.AppHost/Program.cs
@@ -8,6 +8,16 @@ var ollamaEmbeddingModelParameter = builder.Configuration[$"parameters:OllamaEmb
 var ollama = builder.AddOllama("ollama")
     .WithDataVolume();
 
+#pragma warning disable ASPIRECOSMOSDB001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+// Remove "Preview" from the command below to use the stable version of the emulator.
+var cosmos = builder.AddAzureCosmosDB("cosmos-db")
+    .RunAsPreviewEmulator(emulator =>
+    {
+        emulator.WithDataVolume();
+        emulator.WithLifetime(ContainerLifetime.Persistent);
+    });
+#pragma warning restore ASPIRECOSMOSDB001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+
 var chat = ollama.AddModel("chat", ollamaModelParameter!);
 var embeddings = ollama.AddModel("embeddings", ollamaEmbeddingModelParameter!);
 
